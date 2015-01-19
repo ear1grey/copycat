45fdd67bb749a6c4793c4041cb1c37468ecafd8d
David Gauld <dcgauld@gmail.com>
2015-01-19 12:41:38 +0000

    Changed variable.js to reflect new style

---
+++ comparisons/variable.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var _ = require('underscore');
+
+module.exports = function (copycat) {
+
+  var schema1 = copycat.getFile(0).tokenize();
+  var schema2 = copycat.getFile(1).tokenize();
+
+  var variables1 = copycat.utils.filter(schema1, 'identifier');
+  var variables2 = copycat.utils.filter(schema2, 'identifier');
+
+  var variablesArray1 = [];
+  var variablesArray2 = [];
+
+  for (var i = 0; i < variables1.length; i++) {
+    variablesArray1.push(copycat.utils.ngram(variables1[i].value, 2));
+  }
+
+  for (var i = 0; i < variables2.length; i++) {
+    variablesArray2.push(copycat.utils.ngram(variables2[i].value, 2));
+  }
+
+  variables1 = _.flatten(variablesArray1);
+  variables2 = _.flatten(variablesArray2);
+
+  function countSimilarities(a, b) {
+    return a.filter(function(el) {
+      var index = b.indexOf(el);
+      if (index >= 0) {
+        // The element exists in the b array.
+        b.splice(index, 1);
+        return true;
+      }
+      return false;
+    }).length;
+  }
+
+  return parseFloat((countSimilarities(variables1, variables2) / variables1.length) * 100);
+  
+};
