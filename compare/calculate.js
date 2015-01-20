5f78d33168ef2a7c83fb2ef27fdfb90886112a26
David Gauld <dcgauld@gmail.com>
2015-01-20 15:25:31 +0000

    Fixed file adding bug

--- compare/calculate.js
+++ compare/calculate.js
@@ -7,13 +7,14 @@ module.exports = function (file1, file2, comparisons) {
   var results = [];
   var resultTotal = null;
 
+  copycat.addFile(file1);
+  copycat.addFile(file2);
+
   combinations.push([file1, file2]);
   combinations.push([file2, file1]);
 
   for (var i = 0, len = combinations.length; i < len; i++) {
     for (var j = 0, len = comparisons.length; j < len; j++) {
-      copycat.addFile(combinations[i][0]);
-      copycat.addFile(combinations[i][1]);
       var comparison = require('../comparisons/' + comparisons[j].name);
       var result = comparison.call(copycat, copycat);
       results.push({
