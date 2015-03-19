636b8ce37fda1930d18bfbb7f7d97b28943d2d86
David Gauld <dcgauld@gmail.com>
2015-03-19 23:21:59 +0000

    Implemented comment tokens

--- test/5.js
+++ test/5.js
@@ -1,5 +1,12 @@
 if (hello) {
   var hello;
 }
+// This is a test comment
+// To see if it... works?
 var goodbye;
 console.log('hello');
+/*
+This is another comment just to see if it works.
+If this does, it'll be awesome...
+:)
+*/
